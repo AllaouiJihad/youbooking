d481e463d074814014e3632dd18b9a0847621a36
yassir <aginaneyassir@gmail.com>
2023-12-06 21:45:04 +0100

    adjusting the login and sign up

--- include/connexion.php
+++ include/connexion.php
@@ -1,11 +1,7 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', '', 'you_booking');
+$conn = mysqli_connect('localhost', 'root', '', 'youhotels');
 
  if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
    }
-<<<<<<< HEAD
-//   echo "Connected successfully";
-=======
-?>
->>>>>>> ffb0530b62a1f5f7ddcfd978a332c40268eb1437
+
--- include/navbar.php
+++ include/navbar.php
@@ -1,30 +1,37 @@
+<?php
+        $conn=mysqli_connect('localhost','root','','youhotels');
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<!-- Font Awesome -->
-<link
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <!-- Font Awesome -->
+  <link
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
   rel="stylesheet"
-/>
-<!-- Google Fonts -->
-<link
+  />
+  <!-- Google Fonts -->
+  <link
   href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
   rel="stylesheet"
-/>
-<!-- MDB -->
-<link
+  />
+  <!-- MDB -->
+  <link
   href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.css"
   rel="stylesheet"
-/>
-
-<!-- Optional theme -->
-
-<link rel="stylesheet" href="../pages/style.css">
+  />
+  
+  <!-- Optional theme -->
+  
+  <link rel="stylesheet" href="../../youbooking/pages/style.css">
 </head>
 <body>
 <!-- Navbar -->
@@ -32,9 +39,9 @@
   <!-- Container wrapper -->
   <div class="container ">
     <!-- Navbar brand -->
-    <a class="navbar-brand me-2" href="../index.php">
+    <a class="navbar-brand me-2" href="../../youbooking/index.php">
       <img
-        src="../assets/logo.png"
+        src="../../youbooking/assets/logo.png"
         height="40"
         alt="youbooking"
         loading="lazy"
@@ -50,15 +57,47 @@
       </ul>
       <!-- Left links -->
       <div class="d-flex align-items-center">
+      <a href="../../youbooking/pages/login.php">
+        <?php 
+
+        session_start();
+        if(isset($_SESSION['id'])){
+$id=$_SESSION["id"];
+$sql="select * from users where id='$id'";
+$res=mysqli_query($conn,$sql);
+while($rws=mysqli_fetch_assoc($res)){
+  ?>
+    <a class="btn btn-secondary me-2" href="../pages/reqest.php" type="button">become a owner</a>
+  </button>
+  <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle " type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    <?=$rws['firstname']?>
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">profil</a></li>
+    <li><a class="dropdown-item" href="#">my reservation</a></li>
+    <li><a class="dropdown-item" href="#">logout</a></li>
+  </ul>
+</div>
+  
+  <?php
+}
+        }else{
+
+        ?>
       <button data-mdb-ripple-init type="button" class="btn me-3 gold">
          login
-        </button>
+        </button></a>
+        <a href="../../youbooking/pages/signup.php">
         <button data-mdb-ripple-init type="button" class="btn me-3 gold">
           Sign up for free
-        </button>
+        </button></a>
       </div>
     </div>
     <!-- Collapsible wrapper -->
   </div>
+  <?php
+        }
+  ?>
   <!-- Container wrapper -->
 </nav>
